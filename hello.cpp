7e21a3dcc68cca76aa584fce616ff9371f9c62ca
atroopa <atroopa@gmail.com>
2022-04-18 12:46:20 +0430

    i lear string in c++

--- hello.cpp
+++ hello.cpp
@@ -7,12 +7,26 @@ using namespace std;
 int main() 
 {
 
-    int a,b,sum;
-    a = 12;
-    b = 25;
+    string name, food;
+    int    age ; 
+    double money;
 
-    sum = a+b;
-    cout << sum << endl;
+    // Inputing
+
+    cout << "Add a Name: " << endl;
+    cin >> name;
+    cout << "Add a Age: " << endl;
+    cin >> age; 
+    cout << "Add a Food" << endl;
+    cin >> food;
+    cout << "Add Money: " << endl;
+    cin >> money;
+
+
+    cout << name << " is " << age <<" yearls Old! "<< endl;
+    cout <<" He Likes "<< food << " a lot"<< endl;
+    cout << "he has " << money << " in his Card!"<< endl;     
+
 
     return 0;
 
