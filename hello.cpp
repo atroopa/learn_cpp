0a4544c41048b76946ecaa41cc3f2e9f9ab87e3e
atroopa <atroopa@gmail.com>
2022-04-18 12:13:35 +0430

    will apply nasa becuse write print hello world in c++

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+
+    int a,b,sum;
+    a = 12;
+    b = 25;
+
+    sum = a+b;
+    cout << sum << endl;
+
+    return 0;
+
+
+}
