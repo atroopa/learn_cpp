b6869cf695246ad67e5f1aaca03fa03113585020
atroopa <atroopa@gmail.com>
2022-04-19 11:43:59 +0430

    func.cpp created

---
+++ func.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+void print_info(string name)
+{
+
+    cout << "My Name is " << name <<endl;
+
+}
+
+//---------------------------------------------------
+
+int main()
+{
+
+    print_info("Omid Hajavi");
+
+    return 0;
+}
