9e1ea440b565b1c43438a8f02bd2386c845cfe46
atroopa <atroopa@gmail.com>
2022-04-19 12:17:43 +0430

    I learned Functions in c++

--- func.cpp
+++ func.cpp
@@ -1,24 +1,43 @@
+
+// Imports And Includes
 #include <iostream>
 using namespace std;
 
-void print_info(string name, int age);
 
-//---------------------------------------------------
+/////////////////////////////////////////////////////////////////////
+// Functions and Variables Info ------------------------------------/
+/////////////////////////////////////////////////////////////////////
+
+int sdd = 10;
+
+void print_info(string name, int age);
+int  power(int number);
+
+////////////////////////////////////////////////////////////
+// Main ---------------------------------------------------/
+////////////////////////////////////////////////////////////
 
 int main()
 {
 
     print_info("Omid Hajavi", 29);
+    cout << power(7) << endl;
 
     return 0;
 }
 
 
-
+//////////////////////////////////////////////////////////////
+// Functions Source Codes -----------------------------------/
+//////////////////////////////////////////////////////////////
 
 void print_info(string name, int age)
 {
-
     cout << "\n My Name is " << name << " and My Age is " << age << endl;
+}
 
+
+int power(int number)
+{
+    return (number * number + sdd);
 }
